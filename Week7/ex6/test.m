a710b766abfdf8f10a8876423dcfa41d698e94a5
EthanMac <ethanbit@qq.com>
2015-11-01 03:22:57 +0800

    Finalize week7 & starting code for week8

--- Week7/ex6/test.m
+++ Week7/ex6/test.m
@@ -1,8 +1,5 @@
 clear ; close all; clc
 
-load('ex6data3.mat');
-
-[C sigma] = dataset3Params(X, y, Xval, yval);
-
-% model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
-% visualizeBoundary(X, y, model);
+file_contents = readFile('emailSample1.txt');
+word_indices = processEmail(file_contents);
+features = emailFeatures(word_indices);
